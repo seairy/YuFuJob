a95b6b2f5cda82ebbfa0c068c31a7a6c11d7cf89
seairy <seairy@gmail.com>
2015-05-21 15:50:37 +0800

    init commit

--- app/controllers/frontend/cities_controller.rb
+++ app/controllers/frontend/cities_controller.rb
@@ -9,11 +9,13 @@ class Frontend::CitiesController < Frontend::BaseController
     @jobs = @city.jobs.sorted.page(params[:page]).per(40)
     @left_jobs = @jobs.map.each_with_index{|job, i| job if i.even?}.compact
     @right_jobs = @jobs.map.each_with_index{|job, i| job if i.odd?}.compact
+    @title = @city.name
   end
 
   def more
     @jobs = Job.where(city_id: City.unfeatured.map(&:id)).sorted.page(params[:page]).per(40)
     @left_jobs = @jobs.map.each_with_index{|job, i| job if i.even?}.compact
     @right_jobs = @jobs.map.each_with_index{|job, i| job if i.odd?}.compact
+    @title = '更多'
   end
 end
--- app/controllers/frontend/jobs_controller.rb
+++ app/controllers/frontend/jobs_controller.rb
@@ -3,5 +3,6 @@ class Frontend::JobsController < Frontend::BaseController
   def show
     @job = Job.find(params[:id])
     @city = @job.city
+    @title = @job.title
   end
 end
